ac3ccd33007720fecc9758b883aab2ea3f7ecf68
Juliana Stahelin <julianastahelin@gmail.com>
2023-07-15 11:42:45 -0300

    refactor: replace app.css with GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import './App.css';
 import AppRoutes from './routes'; 
 import { ThemeProvider } from './contexts/theme-context';
+import { createGlobalStyle } from 'styled-components';
 
 function App() {
   return (
       <ThemeProvider>
+        <GlobalStyle />
         <AppRoutes />
       </ThemeProvider>
   );
 }
+
+const GlobalStyle = createGlobalStyle`
+
+  body {
+      font-family: 'Geologica', sans-serif;
+      scroll-behavior: smooth;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  li { 
+    list-style-type: none;
+  }
+
+  a { 
+    text-decoration: none;
+    color: #000;
+  }
+`
 
 export default App;
